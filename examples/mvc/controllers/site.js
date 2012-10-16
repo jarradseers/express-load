e8aec2a95f034c7d5114aef843605f5aa600bdae
Jarrad Seers <jarrad@jarradseers.com>
2012-10-16 22:28:54 +1300

    Updated MVC example

--- examples/mvc/controllers/site.js
+++ examples/mvc/controllers/site.js
@@ -1,8 +1,11 @@
+module.exports = function(app) {
 
-exports.index = function(req, res, next) {
   var User = req.app.models.user;
 
-  User.findById(0, function(user) {
-    res.send('Here is a user: ' + user.name + ' - ' + user.email);
-  });
+  this.index = function(req, res, next) {
+    User.findById(0, function(user) {
+      res.send('Have a user: ' + user.name + ' - ' + user.email);
+    });
+  };
+
 };
